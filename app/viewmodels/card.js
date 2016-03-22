8f126844143a40121af6f62a5a23964c6f7b6930
Ashish Charan Tandi <act.jnv@gmail.com>
2016-03-22 20:36:54 +0530

    added {quote} to card

--- app/viewmodels/card.js
+++ app/viewmodels/card.js
@@ -259,6 +259,21 @@ define(['durandal/app', 'knockout', 'jquery', 'lodash', 'card_props', 'mediawiki
             
             // self.update_bind_data_from_card_content(self.bind_data, __card.card_data.card_content);
             //^above is required if card_content comes from some other source after activation is complete
+            //
+            
+            //*******************************//
+            //EXTRAS
+            if(__card.card_data.card_content.text && __card.card_data.card_content.text.toLowerCase().indexOf('{quote}')>-1){
+
+                console.log(__card.card_data.card_content.text);
+                $.getJSON('http://quotes.rest/qod.json', function(json){
+                    if(json.contents && json.contents.quotes.length){
+                        quote = json.contents.quotes[0];
+                        self.bind_data.text("<div style='text-align:center;'>"+quote.quote + '<br><div></div> <div style="text-decoration:overline;">'+ quote.author+"</div></div>");
+                    }
+                    else return;
+                })                
+            }
         };
 
         this.clickedOnElement = function(data, event){
